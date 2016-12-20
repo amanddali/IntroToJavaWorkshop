dd1ffd65b0dbe772ddef2e5c8bc4d5e478281276
RhymeLyet <rhymeinparentheses@gmail.com>
2016-12-20 11:54:10 -0800

    Today we learned about methods and how to use robots to draw using java. We also learned about variables and accurate shapes.

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -6,29 +6,32 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-	
+	Robot queenb= new Robot();
 
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		queenb.setSpeed(10);
 		// 5. Set the pen width to 5
-
+		queenb.setPenWidth(5);
 		// 6. Do steps #7 to #8 four times...
-
+		for (int i = 0; i < 4; i++) {
 			// 7. Set the pen color to random
-	
+			queenb.setRandomPenColor();
 			// 1. Call the drawSquare() method
-	
+			drawSquare();
 			// 8. Turn the robot 90 degrees to the right
-
+			queenb.turn(90);
+		}
 	}
 
 	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
-
-		
-		
+		queenb.penDown();
+		queenb.setRandomPenColor();
+		for (int i = 0; i < 4; i++) {
+		queenb.move(200);
+		queenb.turn(360/4);
+		}
 	}
 
 	public static void main(String[] args) {
--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,25 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
+Robot bender= new Robot();
+	bender.sparkle();
+	bender.setSpeed(20);
+	bender.penDown();
+	bender.setPenColor(100, 0, 170);
+	for (int i = 0; i < 4; i++) {
+		bender.move(200);
+		bender.turn(90);		
+	}
+	bender.penUp();
+	bender.turn(270);
+	bender.move(100);
+	bender.setRandomPenColor();
+	bender.setPenWidth(5);
+	bender.penDown();
+	for (int i = 0; i < 4; i++) {
+		bender.move(450);
+		bender.turn(90);	
+	}
 	
-		
-		
 	}
 }
