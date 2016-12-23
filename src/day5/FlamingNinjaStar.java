9c79d313ad11bac397517f325214d95c8eb4484d
RhymeLyet <rhymeinparentheses@gmail.com>
2016-12-23 11:51:23 -0800

    we created a scary maze and used the robot to make a flaming ninja star

--- src/day5/FlamingNinjaStar.java
+++ src/day5/FlamingNinjaStar.java
@@ -1,5 +1,9 @@
 package day5;
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
+
+import com.sun.beans.editors.ColorEditor;
 
 /*** Teacher's note ***/
  /* Before beginning recipe: 
@@ -17,31 +21,36 @@ public class FlamingNinjaStar {
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 1. Make a new robot, and set it's pen down.
-
+		Robot queenb= new Robot();
+		queenb.setX(1000);
+		queenb.setY(500);
+				queenb.penDown();
 		// 12. Set the robot speed to 10
-
+				queenb.setSpeed(10);
 		// 13. Make all the code below repeat 25 times
-
+				for (int i = 0; i < 25; i++) {
 			// 2. Turn the robot 1/8 of a circle
-		
+				queenb.turn(360/8);
 			// 3. Move the robot 64 pixels
-
+				queenb.move(64);
 			
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+				queenb.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
-			
+				queenb.setPenColor(Color.RED);
+				queenb.move(flameSize);
 			// 6. Turn the robot 170 degrees
-			
+				queenb.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+				queenb.move(flameSize);
 			// 8. Turn the robot 64 degrees to the right
-			
+				queenb.turn(64);
+				queenb.setPenColor(0,0,0);
 			// 9. Move the robot the distance in the variable baseSize
-			
+				queenb.move(flameSize);
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			// 11. Color your ninja star like Figure 2.
-		
+				}
 	}
 
 }
--- src/day5/ScaryMaze.java
+++ src/day5/ScaryMaze.java
@@ -12,6 +12,7 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
@@ -21,15 +22,11 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	final int frameWidth = 600;
 	final int frameHeight = 400;
 
+
 	ScaryMaze() throws Exception {
-		//1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
-		maze = ImageIO.read(getClass().getResource("maze.png"));
-		//2. set the mouse pointer to the start of your maze using:
-		//new Robot().mouseMove(xPosition, yPosition)
-		
-		//3. add a mouse motion listener using:
-		//addMouseMotionListener(this)
-		
+		maze = ImageIO.read(getClass().getResource("SCARY MAZE.jpg"));
+		new Robot().mouseMove(30,80);
+		addMouseMotionListener(this);
 	}
 
 	@Override
@@ -38,28 +35,31 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
 		//4. print the mouseColor variable to see what color the mouse is touching
-
+		System.out.println(mouseColor);
 		//5. make a variable to hold the background color. 
-
+		int backgroundColor = -1;
 		//6. if the mouse falls off the path (if it is on the background)
-		
-				// call the scare method
-		
+		if (mouseColor==backgroundColor) {
+			scare();
+		}
+		int endColor = -16513872;
 		//10. if the mouse is on the end color
-				
-				// pop up a message to tell them they won
+		if (mouseColor==endColor) {
+			JOptionPane.showMessageDialog(null,"You have made it through the maze of possessed children");
+		}
+			
 		
 	}
 
 	private void scare() {
 		System.out.println("BOO!");
 		//7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
-		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("scaresound.wav"));
 		
 		//8. play the scary sound. Hint: type "sound" and then a period.		
-		
+		sound.play();
 		//9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
-
+		showScaryImage("scaryimage.jpg");
 	}
 
 	private void showScaryImage(String imageName) {
@@ -77,7 +77,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 
 	@Override
 	public void run() {
-		JFrame frame = new JFrame("June's Scary Maze");
+		JFrame frame = new JFrame("Maze of Possessed Childred");
 		frame.add(this);
 		setPreferredSize(new Dimension(frameWidth, frameHeight));
 		frame.pack();
